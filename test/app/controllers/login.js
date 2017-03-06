c8df0de829243d0666a220f1076dd61879df8c7b
ivan <ivan@tideanalytics.com>
2017-03-06 18:23:13 +0800

    +ve & -ve test case on login

--- test/app/controllers/login.js
+++ test/app/controllers/login.js
@@ -1,6 +1,7 @@
 'use strict';
 
 const async    = require('async');
+const uuid = require('uuid/v4')
 const chai     = require('chai');
 const chaiHttp = require('chai-http');
 const app      = require('../../../app');
@@ -11,7 +12,7 @@ chai.use(chaiHttp);
 
 describe('/login', () => {
 
-  let user;
+  let user, key;
 
   before(done => {
 
@@ -19,32 +20,50 @@ describe('/login', () => {
       helpers.database,
       helpers.credentials,
       helpers.login
-    ], (err, key, generatedUser) => {
-      console.log(err, key, generatedUser);
+    ], (err, generatedKey, generatedUser) => {
+      key  = generatedKey;
+      user = generatedUser;
       done();
     });
 
   });
 
 
-  it('should do something' , (done) => {
+  it('should be able to login properly' , (done) => {
 
     const username = user.username;
-    const password = 'test-password';
+    const password = user.password;
 
     chai
       .request(app)
       .post('/login')
       .send({username, password})
-      .exec( (err, resp) => {
-        console.log(resp);
+      .then( response => {
+        expect(response.body).to.exist;
         done();
-      });
+      })
+      .catch(done);
+  });
+
+  it('should deny access to unknown users', (done) => {
+
+    const username = uuid();
+    const password = uuid();
+
+    chai
+      .request(app)
+      .post('/login')
+      .send({username, password})
+      .then( response => {
+      })
+      .catch(err => {
+        expect(err).to.exist;
+        expect(err.status).to.equal(401);
+        expect(err.response.error.text).to.equal('invalid ccredentials');
+        done();
+      })
 
   });
 
-  after(done => {
-    done();
-  });
 
 });
